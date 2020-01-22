ba664b71f64eaa52c1ee7fb7c51f1e0a3a661281
Олексій Пишняк <Pyshnyak@rivne.itstep.org>
2020-01-22 20:18:10 +0200

    Added table list

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-let url='https://poloniex.com/public?command=returnCurrencies';
-async function getData(){
-    let request=await fetch(url);
-    let json=await request.json().catch();
-    console.log(json);
-    return json;
+let url = 'https://poloniex.com/public?command=returnCurrencies';
+async function getData() {
+    let request = await fetch(url);
+    let json = await request.json().catch();
+    $(function () {
     
-    }
+        let table = $('#mainTable');
+        let str=[];
+        for (let k in json) {
+            str.push( '<tbody><tr><td scope="row">'
+                + json[k].id + '</td><td>'
+                + json[k].humanType + '</td><td>'
+                + json[k].name + '</td><td>'
+                + json[k].currencyType + '</td><td>'
+                + json[k].txFee + '</td><td>'
+                + json[k].minConf + '</td>'+
+                '<td><button type="button" class="btn btn-dark" onclick="Delete()">Delete</button></td></tr>/tbody>');
+        }
+        table.append(str);
+    });
+    return json;
+}
 
-
-let data=getData();
-
-$(function () {
-    let t = $('#mainTable');
-    let counter = 1;   
-    let str='<tbody><tr><td scope="row">'+data.id+'</td><td>'+data.name+'</td><td>'+data.humanType+'</td><td>'+data.currencyType+'</td><td>0.'+data.txFee+'</td><td>'+data.minConf+'</td></tr>/tbody>';
-    t.append(str);
-});
-
-
-
+getData();
